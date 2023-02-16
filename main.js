075017e0271b615a38942742ba70e5719cecfe47
vignesh20-fed <svignesh2000tsi@gmail.com>
2023-02-16 09:12:10 +0530

    update

---
+++ main.js
@@ -0,0 +1,287 @@
+let foodList=[
+    {
+	 id:1,
+	 name:"Hotel Sarvana Bhavan",
+	 cusine:"Veg,Non-Veg,Chicken Rice,Noodles",
+	 Rating:"4.2",
+	 time:'<a herf="#">53 MIN</a>',
+	 price:"350 FOR TWO",
+	 dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 view:'<a herf="#">Quick View</a>',	 
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/b864a70c509ac70c3107deda2aebba85"
+    },
+	    {
+	 id:2,
+	 name:'Aasiffe Briyani',
+	 cusine:'Chinese,Chicken Rice,Noodles',
+	 Rating:4.2,
+	 time:"53 MIN",
+	  	 dot:".",
+	 dot2:".",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ikey4p2ievbda5b2x7hn"
+    },
+	    {
+	 id:3,
+	 name:'Hotel Grill Land',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  dot:".",
+	 dot2:".",
+	 time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hrzzefyekcewtpemua7j"
+    },
+	    {
+	 id:4,
+	 name:'Hotel Anantha Bhavan',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  dot:".",
+	 dot2:".",
+	 time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rcwgyuqxted668ehy1jo"
+    },
+	    {
+	 id:5,
+	 name:'New Rusi Restaurant',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  dot:".",
+	 dot2:".",
+	 time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/eigifgugxsm1doctoesa"
+    },
+	    {
+	 id:6,
+	 name:'Hot N Spot',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	 time:"53 MIN",
+	 price:"350 FOR TWO",
+	  dot:".",
+	 dot2:".",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ii6enfkxe6uwbvw3qiar"
+    },
+	  {
+	 id:7,
+	 name:"Snack Time",
+	 cusine:"Parotta,Chicken Rice,Noodles,Chinese",
+	 Rating:4.2,
+	 time:"53 MIN",
+	  dot:".",
+	 dot2:".",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/acbj9ahsqpuczxrgrcab"
+    },
+	    {
+	 id:8,
+	 name:'Taj Briyani',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	 time:"53 MIN",
+	  dot:".",
+	 dot2:".",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/kafogzob7zahwzkkksrp"
+    },
+	    {
+	 id:9,
+	 name:'Fried N Fiery',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  time:"53 MIN",
+	 price:"350 FOR TWO",
+	  dot:".",
+	 dot2:".",
+	  view:"Quick View",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/zqh2geicmcgdrveo3rdp"
+    },
+	    {
+	 id:10,
+	 name:'Hotel Sri Devi Bhavan',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	   dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/o44whpjdqfdcyveid7aj"
+    },
+	    {
+	 id:11,
+	 name:'New Rusi Restaurant',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	 time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	   dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qkuumbeiw5eewtfibpg3"
+    },
+	    {
+	 id:12,
+	 name:'Hot N Spot',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	   dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/tqog1cizaayqgbnjdou2"
+    },
+		    {
+	 id:13,
+	 name:'Pizza Corner',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	 time:"53 MIN",
+	 price:"350 FOR TWO",
+	 view:"Quick View",
+	 dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/itcbgkjokxfmyehjoyzw"
+    },
+		    {
+	 id:14,
+	 name:'Maggi Restaurant',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	   dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/p6vfhg7cdvwafggrkcuc"
+     },
+		    {
+	 id:15,
+	 name:'Lunch Box',
+	 cusine:'Parotta,Chicken Rice,Noodles,Chinese',
+	 Rating:4.2,
+	  time:"53 MIN",
+	 price:"350 FOR TWO",
+	  view:"Quick View",
+	   dot:".",
+	 dot2:".",
+	 offer:"50% Off | Use WELCOME50",
+	 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/nrjmqrlhznvmzaqqq14p"
+    }
+]
+
+
+
+
+let cont= document.querySelector(".container");
+let cards= document.createElement("div");
+cards.classList.add("row");
+cont.appendChild(cards);
+
+                     /*-------------------------------------------- col ----------------------------------------------------*/
+foodList.forEach(function(e){
+let div=document.createElement("div");
+div.classList.add("col");
+cards.appendChild(div);
+                     /*-------------------------------------------- col-card ----------------------------------------------------*/
+let div2=document.createElement("div");
+div2.classList.add("col-card");
+div2.style.background="#ffffffcf";
+div.appendChild(div2);
+
+let img = document.createElement("img");
+img.src=e.imgUrl;
+div2.appendChild(img);
+
+let head=document.createElement("h3");
+head.innerText = e.name;
+head.style.margin="10px";
+div2.appendChild(head);
+
+let para=document.createElement("p");
+para.innerText=e.cusine;
+para.style.margin="10px";
+para.style.color="#585656";
+div2.appendChild(para);	
+                        /*-------------------------------------------- col-con ----------------------------------------------------*/
+let div3= document.createElement("div");
+div3.classList.add("col-con");
+div3.style.margin="10px";
+div2.appendChild(div3);
+
+let but=document.createElement("p");
+but.classList.add("icon");
+but.innerHTML=` <i class="fa fa-star" aria-hidden="true"></i> ${e.Rating}`;
+but.style.backgroundColor="#64f578";
+but.style.border="none";
+but.style.color="white";
+div3.appendChild(but);	
+
+let dot=document.createElement("p");
+dot.innerText=e.dot;
+dot.style.fontWeight="bold";
+div3.appendChild(dot);	
+
+let time=document.createElement("p");
+time.innerHTML=e.time;
+div3.appendChild(time);	
+
+let dot2=document.createElement("p");
+dot2.innerText=e.dot2;
+dot2.style.fontWeight="bold";
+div3.appendChild(dot2);	
+
+let price=document.createElement("p");
+price.innerHTML=`<i class="fa fa-inr" aria-hidden="true"></i>${e.price}`;
+div3.appendChild(price);	
+                        /*-------------------------------------------- offer ----------------------------------------------------*/
+let div4= document.createElement("div");
+div4.classList.add("offer");
+div2.appendChild(div4);
+
+let offer=document.createElement("p");
+offer.innerHTML=`<iconify-icon icon="bxs:offer"></iconify-icon> ${e.offer}`;
+offer.style.color="brown";
+offer.style.margin="10px";
+div4.appendChild(offer);	
+                         /*-------------------------------------------- hover ----------------------------------------------------*/
+let div5=document.createElement("div");
+div5.classList.add("hover");
+div2.appendChild(div5);
+
+let hover=document.createElement("p");
+hover.innerHTML=e.view;
+hover.style.color="blue";
+hover.style.fontSize="20px";
+div5.appendChild(hover);	
+
+	
+});
+
+console.log(cont)
